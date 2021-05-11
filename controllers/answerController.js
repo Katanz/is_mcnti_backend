a8cc3fddf8915ac89e154e5472ea661e833b3bd2
Andrey Sivov <drao.1338@gmail.com>
2021-05-11 18:48:28 +0300

    add methods to controller

--- controllers/answerController.js
+++ controllers/answerController.js
@@ -1,6 +1,15 @@
+const {Answer} = require('../models/models')
+const ApiError = require('../error/ApiError')
+
 class AnswerController {
-  async create(req, res) {}
-  async getAll(req, res) {}
+  async create(req, res) {
+    const {answer} = req.body,
+    const answerSave = await Answer.create({answer})
+    return res.json(answerSave)
+  }
+  async getAll(req, res) {
+    const answers = await Answer.findAll
+  }
   async getOne(req, res) {}
   async delete(req, res) {}
 }
