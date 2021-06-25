1de101a176b76025b4f5967f5d2902e0dd33a470
Andrey Sivov <drao.1338@gmail.com>
2021-06-25 16:47:58 +0300

    change route names

--- routes/roleRoute.js
+++ routes/roleRoute.js
@@ -2,7 +2,7 @@ const Router = require('express')
 const router = new Router()
 const roleController = require('../controllers/RoleController')
 
-router.post('/add-role', roleController.create)
+router.post('/', roleController.create)
 router.get('/', roleController.getAll)
 router.delete('/role/:id', roleController.delete)
 
